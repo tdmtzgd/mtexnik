6c67b75e491cb56c56afe0307953943d4fa390f8
tdmtzgd <54685415+tdmtzgd@users.noreply.github.com>
2019-08-29 22:22:36 +0300

    Add files via upload

---
+++ srv160174.hoster-test.ru/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://srv160174.hoster-test.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://srv160174.hoster-test.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/srv160174/srv160174.hoster-test.ru/catalog/');
+define('DIR_SYSTEM', '/home/srv160174/srv160174.hoster-test.ru/system/');
+define('DIR_IMAGE', '/home/srv160174/srv160174.hoster-test.ru/image/');
+define('DIR_STORAGE', '/home/srv160174/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'srv160174_pkyjn');
+define('DB_PASSWORD', 'QXT8HyLz');
+define('DB_DATABASE', 'srv160174_g78yx');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
